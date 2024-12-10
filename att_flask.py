c9947f7e370c08ced1b1d885655554b697c339ec
Taha Mohamed Taha <dr.taha.1998@gmail.com>
2024-12-10 18:51:10 +0200

    removed wrong comment to fix download issue

--- att_flask.py
+++ att_flask.py
@@ -9,7 +9,7 @@ def index():
     if request.method == 'POST':
         url = request.form.get('url')
         file_id=extract_file_id(url)
-        #att_table=attendance_main(url)   
+        att_table=attendance_main(url)   
         return render_template('submit.html', url_input=url, file_id=file_id) #, result=att_table)
     return render_template('index.html')
 
